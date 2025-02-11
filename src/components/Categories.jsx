222ac0495d004d7b816aa8116be7e85954818d56
jonorl <39122230+jonorl@users.noreply.github.com>
2025-02-11 16:02:06 +0300

    Feat: Electronics being fetched and displayed correctly

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,32 +1,42 @@
 import { useEffect, useState } from "react";
+import { useParams } from 'react-router-dom';
 
-const Categories = () => {
+function Categories() {
+    const { name } = useParams();
+    console.log(name)
+    const [category, setCategory] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-        const [category, setCategory] = useState(null);
-        const [error, setError] = useState(null);
-        const [loading, setLoading] = useState(true);
-    
-        useEffect(() => {
-            fetch("https://fakestoreapi.com/products/categories", { mode: "cors" })
-                .then((response) => {
-                    if (response.status >= 400) {
-                        throw new Error("server error");
-                    }
-                    return response.json();
-                })
-                .then((response) => setCategory(response))
-                .catch((error) => setError(error))
-                .finally(() => setLoading(false));
-        }, []);
-    
-        if (loading) return <p>Loading...</p>;
-        if (error) return <p>A network error was encountered</p>;
+    useEffect(() => {
+
+        fetch(`https://fakestoreapi.com/products/${typeof(name) !== 'undefined' ? 'category/' + name.toLowerCase() : 'categories'}`, { mode: "cors" })
+            .then((response) => {
+                if (response.status >= 400) {
+                    throw new Error("server error");
+                }
+                return response.json();
+            })
+            .then((response) => setCategory(response))
+            .catch((error) => setError(error))
+            .finally(() => setLoading(false));
+    }, [name]);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>A network error was encountered</p>;
     return (
-        <div className="categories-main-dropdown">
-            {category.map((cat, index) => (
-                <ul key={index}>{cat}</ul>
-            ))}
-        </div>
+        <>
+            <h2>Category: {name}</h2>
+            {!loading && category && category.length > 0 ? (
+            <div className="categories-main-dropdown">
+                {category.map((cat, index) => (
+                    <ul key={index}>{cat.title}</ul>
+                ))}
+            </div>
+            ) : !loading && category && category.length === 0 ? (
+                <div>No cat selected</div>
+            ): null}
+        </>
     );
 };
 
