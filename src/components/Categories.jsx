ba8da3dc2c26da95808d56a35eea27cce51d5b99
jonorl <39122230+jonorl@users.noreply.github.com>
2025-02-12 11:10:40 +0300

    Feat: Links to categories now working: Fix (WIP): Hitting the back button is now working but the ternary is too complex. Need to simplify it

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,16 +1,16 @@
 import { useEffect, useState } from "react";
 import { useParams } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Categories() {
     const { name } = useParams();
-    console.log(name)
     const [category, setCategory] = useState(null);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
 
-        fetch(`https://fakestoreapi.com/products/${typeof(name) !== 'undefined' ? 'category/' + name.toLowerCase() : 'categories'}`, { mode: "cors" })
+        fetch(`https://fakestoreapi.com/products/${typeof (name) !== 'undefined' ? 'category/' + name.toLowerCase() : 'categories'}`, { mode: "cors" })
             .then((response) => {
                 if (response.status >= 400) {
                     throw new Error("server error");
@@ -27,15 +27,19 @@ function Categories() {
     return (
         <>
             <h2>Category: {name}</h2>
-            {!loading && category && category.length > 0 ? (
-            <div className="categories-main-dropdown">
-                {category.map((cat, index) => (
-                    <ul key={index}>{cat.title}</ul>
-                ))}
-            </div>
-            ) : !loading && category && category.length === 0 ? (
-                <div>No cat selected</div>
-            ): null}
+            {!loading && (typeof (name) !== "undefined" ? (
+                <div className="categories-main-dropdown">
+                    {category.map((cat, index) => (
+                        <ul key={index}>{cat.title}</ul>
+                    ))}
+                </div>
+            ) : category && typeof category[0] === 'string' ? (
+                <div className="categories-main-dropdown">
+                    {category.map((cat, index) => (
+                        <Link key={index} to={cat}>{cat}</Link>
+                    ))}
+                </div>
+            ) : <p>Loading...</p>)}
         </>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from "react-router-dom";
+import router from './components/Routes.jsx';
 import './styles/index.css'
-import App from './components/App.jsx'
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
