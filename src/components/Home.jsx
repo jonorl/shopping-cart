f95b20c18b7a2346a32c888d72f8714119482bfc
jonorl <39122230+jonorl@users.noreply.github.com>
2025-02-14 16:18:59 +0300

    Fix: Moved static image to public directorate

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ export default function Center() {
     return (
         <div className="center">
             <h1 className="home-title">Welcome to my mock shopping website!</h1>
-            <img className="dog" src="./src/assets/images/istockphoto-828399432-612x612.jpg" alt="Picture of a Wired haired Fox Terrier" />
+            <img className="dog" src='../../public/assets/images/istockphoto-828399432-612x612.jpg' alt="Picture of a Wired haired Fox Terrier" />
             <p className="home-description">Here goes the home main message, like welcome to the site, etc.</p>
         </div>
     )
